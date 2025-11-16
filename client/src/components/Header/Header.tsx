195324b452254199ef469a61ec3d99e6dbf7eff8
AlboLock <albertolucchetti@gmail.com>
2025-11-16 10:51:27 +0100

    Initial project commit

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div>
+          <div className="logoContainer">
+            <img src="images/logo.png" alt="Logo" />
+          </div>
+          <div>
+            <h2>Welcome, User</h2>
+          </div>
+        </div>
+        <div className="navContainer">
+          <span>Dashboard</span>
+          <span>My Reports</span>
+          <span>Map</span>
+        </div>
+        <div>
+          <div className="profile-container">
+            <span>Nombre Apellido</span>
+            <span>
+              <img src="images/profile-icon.jpg" alt="Profile Image" />
+            </span>
+          </div>
+          <div className="new-report-container">
+            <button>New report</button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
